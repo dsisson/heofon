165ed9032a62ea2d5df575e721a283232ce68f1d
dsisson <philosophe@gmail.com>
2024-06-09 10:45:15 -0400

    support pom booting
    apps/sweetshop/base_page.py
    + added PomBootPage
    
    tests/sweetshop/test_sweetshop.py
    + added PomBootPage step to both test methods

--- heofon/tests/sweetshop/test_sweetshop.py
+++ heofon/tests/sweetshop/test_sweetshop.py
@@ -4,11 +4,13 @@ import re
 from playwright.sync_api import Page, expect
 
 from heofon.framework import utils
-from heofon.apps.sweetshop.noauth import pages as pom
+from heofon.apps.sweetshop.base_page import PomBootPage
 
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.example
+@pytest.mark.playwright
 class SweetshopTests:
 
     def test_linear_navigation(self, pwpage, sweetshop):
@@ -28,30 +30,39 @@ class SweetshopTests:
         # we have a webdriver instance from this method's fixture `driver`,
         # which corresponds to the "browser" argument at the CLI invocation
 
+        # instantiate the POM on the blank driver start page
+        boot_page = PomBootPage(pwpage)
+
         # instantiate the home page object by calling the PO
         # with the playwright browser page instance
-        home_page = pom.HomePage(pwpage)
-        home_page.save_screenshot('home page')
+        home_page = boot_page.start_with('sweetshop home page')
+        home_page.save_screenshot('home loaded')
+        assert pwpage.title() == home_page.title
 
         id = 'Sweets'
         sweets_page = home_page.top_menu_goto(id)
         sweets_page.save_screenshot(f"{id} page")
+        assert pwpage.title() == sweets_page.title
 
         id = 'About'
         about_page = sweets_page.top_menu_goto(id)
         about_page.save_screenshot(f"{id} page")
+        assert pwpage.title() == about_page.title
 
         id = 'Login'
         login_page = about_page.top_menu_goto(id)
         login_page.save_screenshot(f"{id} page")
+        assert pwpage.title() == login_page.title
 
         id = 'Basket'
         basket_page = login_page.top_menu_goto(id)
         basket_page.save_screenshot(f"{id} page")
+        assert pwpage.title() == basket_page.title
 
         id = 'Home'
         home_page = login_page.top_menu_goto(id)
         home_page.save_screenshot(f"{id} page again")
+        assert pwpage.title() == home_page.title
 
     @pytest.mark.parametrize('scenario',
                              [
@@ -83,12 +94,17 @@ class SweetshopTests:
         msg = f"testing navigation path: \nHome --> {'--> '.join(scenario)}"
         logger.info(f"\n{'#' * 60}\n{msg}\n{'#' * 60}\n")
 
+        # instantiate the POM on the blank driver start page
+        boot_page = PomBootPage(pwpage)
+
         # instantiate the home page object by calling the PO
         # with the playwright browser page instance
-        page = pom.HomePage(pwpage)
-        page.save_screenshot('home page')
+        page = boot_page.start_with('sweetshop home page')
+        page.save_screenshot('home loaded')
+        assert pwpage.title() == page.title
 
         for destination in scenario:
             logger.info(f"\nnavigating to {destination} page")
             page = page.top_menu_goto(destination)
             page.save_screenshot(f"{destination.lower()} page loaded")
+            assert pwpage.title() == page.title
